c9a873791e039525f23d7e2eb54796a81e749fa3
Abdalrhman Gamal <abdalrhmangamel681@gmail.com>
2024-03-28 00:03:48 +0200

    New feature: Add emailjs package

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MessageService } from 'primeng/api';
+import emailjs from '@emailjs/browser';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -16,17 +17,23 @@ export class ContactComponent implements OnInit {
   })
 
   constructor(private messageService: MessageService) { }
-
   ngOnInit(): void {
     $('.Contact').fadeOut(0).fadeIn(600);
   }
   showSuccess() {
-    this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Thank for your message' });
+    this.messageService.add({ severity: 'success', summary: 'Message Sended', detail: 'Thank for your message I will contact you soon' });
   }
-  onSubmit(form: FormGroup) {
-    if (form.valid) {
-      this.showSuccess();
-      form.reset();
-    }
+  async sendEmail() {
+    emailjs.init("OBd9AtgMZJbmFzwY3");
+    await emailjs.send("service_ssorf74", "template_bwv2qfq", {
+      from_name: this.contactForm.value.name,
+      to_name: "Abdalrhman Gamal",
+      subject: this.contactForm.value.subject,
+      message: this.contactForm.value.message,
+      from_email: this.contactForm.value.email,
+    });
+
+    this.showSuccess();
+    this.contactForm.reset();
   }
 }
